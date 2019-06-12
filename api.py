6637eb05377f47cda64e08100c4ef069df13e103
Streand <Eirik.strand1996@hotmail.com>
2019-06-12 10:29:49 +0200

    update

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
 
 app = Flask(__name__)
 
@@ -69,11 +71,45 @@ def create_user():
 
 @app.route('/user/<public_id>', methods=['PUT'])
 def promote_user(public_id):
-    return ''
+
+    user = User.query.filter_by(public_id=public_id).first()
+    
+    if not user:
+        return jsonify({'message' : 'User not found!'})
+
+    user.admin = True
+    db.session.commit()
+
+    return jsonify({'message' : 'The user has been promoted'})
 
 @app.route('/user/<public_id>', methods=['DELETE'])
 def delete_user(public_id):
-    return ''
+
+    user = User.query.filter_by(public_id=public_id).first()
+    
+    if not user:
+        return jsonify({'message' : 'User not found!'})
+
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message' : 'The user has been deleted!'})
+
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!'})
+
+    user = User.query.filter_by(name=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+    return jsonify({'token' : token.decode('UTF-8')})
 
 if __name__ == '__main__':
     app.run(debug=True)
